15bdf49cff0e05da8f52feb383c7f2e4b9841b56
catzy007 <catzy.rais@gmail.com>
2020-05-23 16:56:44 +0700

    some update

--- starter.sh
+++ starter.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 devName="MyVirtualCam"
 
+if [ "$EUID" -ne 0 ]
+  then echo "Cannot set some parameters"; echo "Are you root?"
+  exit
+fi
+
 FILES=/dev/video*
 isDevAvail=0
 for f in $FILES
@@ -13,10 +18,10 @@ do
 done
 
 if (( isDevAvail == 0 )); then
-    echo "Assigning device"
-    sudo modprobe v4l2loopback devices=1 card_label="$devName" exclusive_caps=1,1,1,1,1,1,1,1
+    echo "Assigning $devName"
+    sudo modprobe v4l2loopback devices=1 card_label=$devName exclusive_caps=1,1,1,1,1,1,1,1
 else
-    echo "Device allready assigned"
+    echo "$devName allready assigned"
 fi
 
 for f in $FILES
@@ -24,9 +29,9 @@ do
     #echo $(echo "$f" | cut -c6-)
     #cat /sys/class/video4linux/$(echo "$f" | cut -c6-)/name
     if [ "$(cat /sys/class/video4linux/$(echo "$f" | cut -c6-)/name)" == "$devName" ]; then
-        echo "Starting Virtual Cam from $f"
-        #ffmpeg -f flv -listen 1 -i rtmp://localhost:1935/virtualCam -f v4l2 -vcodec rawvideo $f
+        echo "Starting $devName from $f"
         echo "Set OBS stream server as rtmp://localhost:1935/virtualCam and Start Streaming"
+        ffmpeg -f flv -listen 1 -i rtmp://localhost:1935/virtualCam -f v4l2 -vcodec rawvideo $f
     fi
 done
 
